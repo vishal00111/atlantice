387315cd31c71822a14bdcffb181d969d0ef8990
vishal00111 <acsvishal53@gmail.com>
2025-05-17 20:46:08 +0530

    first commit

--- src/components/header.js
+++ src/components/header.js
@@ -14,7 +14,7 @@ const BanquetHeroSection = () => {
         <ul className="nav-links">
           <li><a href="#home">Home</a></li>
           <li><a href="#gallery">Gallery</a></li>
-          <li><a href="#packages">Packages</a></li>
+          <li><a href="#packages">Packagess</a></li>
           <li><a href="#contact">Contact</a></li>
           <li><a href="#menu"><i className="menu-icon">☰</i></a></li>
         </ul>
