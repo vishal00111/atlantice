27b2f6839d3fe93ac0730749c4982a0c5d7d1ec3
vishal00111 <acsvishal53@gmail.com>
2025-05-19 03:58:44 +0530

    Your message for the update

--- src/components/wedding/wedding.js
+++ src/components/wedding/wedding.js
@@ -16,7 +16,7 @@ const WeddingExperience = () => {
             WHERE EVERY MOMENT SHINES!
           </h2>
           <p>
-            Experience a seamless, one-stop wedding service at Heritage Village Resorts & Spa, Manesar—one of the premier wedding resorts near Delhi. Our hand-picked network of leading photographers, wedding planners, salon experts, decorators, and more work together to bring your vision to life.
+            Experience a seamless, one-stop wedding service at Heritage Village Resorts & Spa, Manesar—one of the premier wedding resorts . Our hand-picked network of leading photographers, wedding planners, salon experts, decorators, and more work together to bring your vision to life.
           </p>
           <p>
             From intimate Roka and Haldi ceremonies to elaborate Mehendi, cocktail receptions, Sangeet parties, and grand wedding receptions, our dedicated team delivers customised packages and meticulously crafted menus for every function. Relax and enjoy a stress-free wedding journey, knowing every detail is handled with utmost care.
